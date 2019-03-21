e345ee703fe02bc069b2afb2d7d40bc9c81feca4
Matt Kieblesz <mattkieblesz@hotmail.com>
2019-03-21 16:41:39 +0000

    Add submission list view

--- submission/views.py
+++ submission/views.py
@@ -1,6 +1,7 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import ListCreateAPIView
 
 from submission import constants, serializers, tasks
+from submission.models import Submission
 
 from client.authentication import ClientSenderIdAuthentication
 
@@ -25,9 +26,18 @@ action_map = {
 }
 
 
-class SubmissionCreateAPIView(CreateAPIView):
+class SubmissionCreateAPIView(ListCreateAPIView):
     serializer_class = serializers.SubmissionModelSerializer
     authentication_classes = [ClientSenderIdAuthentication]
+
+    def get_queryset(self):
+        sso_user_id = self.request.query_params['sso_user_id']
+        if not sso_user_id or not sso_user_id.isnumeric():
+            return Submission.objects.none()
+
+        return Submission.objects.filter(
+            meta__sender__sso_user_id=int(sso_user_id)
+        ).order_by('-created')
 
     def perform_create(self, serializer):
         super().perform_create(serializer)

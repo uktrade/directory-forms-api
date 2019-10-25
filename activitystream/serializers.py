7e01b280e366fd6d9663edde3e5664259908bb56
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-10-25 19:30:58 +0100

    add NS constant

--- activitystream/serializers.py
+++ activitystream/serializers.py
@@ -3,29 +3,31 @@ from rest_framework import serializers
 
 class SubmissionSerializer(serializers.Serializer):
 
+    DIT_NAMESPACE = 'dit:directoryFormsApi'
+
     def to_representation(self, obj):
         return {
-            'id': f'dit:directoryFormsApi:Submission:{obj.id}:Create',
+            'id': f'{self.DIT_NAMESPACE}:Submission:{obj.id}:Create',
             'type': 'Create',
             'published': obj.created.isoformat('T'),
             'actor': {
-                'type': 'dit:directoryFormsApi:Submission:Sender',
-                'id': f'dit:directoryFormsApi:Sender:{obj.id}',
+                'type': f'{self.DIT_NAMESPACE}:Submission:Sender',
+                'id': f'{self.DIT_NAMESPACE}:Sender:{obj.id}',
                 'dit:emailAddress': obj.sender.email_address,
                 'dit:isBlacklisted': obj.sender.is_blacklisted,
                 'dit:isWhitelisted': obj.sender.is_whitelisted,
                 'dit:blackListedReason': obj.sender.blacklisted_reason,
             },
             'object': {
-                'type': 'dit:directoryFormsApi:Submission',
-                'id': f'dit:directoryFormsApi:Submission:{obj.id}',
-                'dit:directoryFormsApi:Submission:Meta': obj.meta,
-                'dit:directoryFormsApi:Submission:Data': obj.data,
+                'type': f'{self.DIT_NAMESPACE}:Submission',
+                'id': f'{self.DIT_NAMESPACE}:Submission:{obj.id}',
+                f'{self.DIT_NAMESPACE}:Meta': obj.meta,
+                f'{self.DIT_NAMESPACE}:Data': obj.data,
                 'published': obj.created.isoformat('T'),
                 'name': obj.form_url,
                 'attributedTo': {
-                    'type': f'dit:directoryFormsApi:SubmissionAction:{obj.action_name}',
-                    'id': f'dit:directoryFormsApi:SubmissionType:{obj.submission_type}'
+                    'type': f'{self.DIT_NAMESPACE}:SubmissionAction:{obj.action_name}',
+                    'id': f'{self.DIT_NAMESPACE}:SubmissionType:{obj.submission_type}'
                 },
             },
         }

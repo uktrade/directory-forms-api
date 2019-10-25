eb4e17e373edabca6dbdf06ae3904267f1b26fe0
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-10-25 15:50:14 +0100

    serialiser changes

--- activitystream/serializers.py
+++ activitystream/serializers.py
@@ -7,23 +7,26 @@ class SubmissionSerializer(serializers.Serializer):
 
     def to_representation(self, obj):
         return {
-            'id': f'dit:directoryFormsApi:Submission:{obj.id}:published',
-            'type': 'published',
-            'published': obj.created.isoformat('T'),
+            'id': f'dit:directoryFormsApi:Submission:{obj.id}:Create',
+            'type': 'Create',
+            'Create': obj.created.isoformat('T'),
+            'actor': {
+                'type': 'dit:directoryFormsApi:Submission:Sender',
+                'dit:directoryFormsApi:Submission:Sender': SenderSerializer(obj.sender).data,
+            },
+
              'object': {
                 'type': 'dit:directoryFormsApi:Submission',
                 'id': f'dit:directoryFormsApi:Submission:{obj.id}',
-                'dit:directoryFormsApi:Submission:sender': SenderSerializer(obj.sender).data,
-                'dit:directoryFormsApi:Submission:client': ClientSerializer(obj.client).data,
-                'dit:directoryFormsApi:Submission:meta': obj.meta,
+                'dit:directoryFormsApi:Submission:Client': ClientSerializer(obj.client).data,
+                'dit:directoryFormsApi:Submission:Meta': obj.meta,
                 'content': obj.data,
                 'published': obj.created,
-                'url': obj.form_url,
+                'name': obj.form_url,
                 'attributedTo': {
                     'type': f'dit:directoryFormsApi:SubmissionAction:{obj.action_name}',
                     'id': f'dit:directoryFormsApi:SubmissionType:{obj.type}'
                 },
-
             },
         }
 
@@ -32,10 +35,10 @@ class SenderSerializer(serializers.Serializer):
     def to_representation(self, obj):
         return {
             'id': f'dit:directoryFormsApi:Sender:{obj.id}',
-            'email_address': obj.email_address,
-            'is_blacklisted': obj.is_blacklisted,
-            'is_whitelisted': obj.is_whitelisted,
-            'blacklisted_reason': obj.blacklisted_reason,
+            'dit:emailAddress': obj.email_address,
+            'dit:isBlacklisted': obj.is_blacklisted,
+            'dit:isWhitelisted': obj.is_whitelisted,
+            'dit:blackListedReason': obj.blacklisted_reason,
         }
 
 

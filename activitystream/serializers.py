3d555c6a0a928ca8d951ae5165697748573520f5
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-10-25 20:26:12 +0100

    add Sender serialiser

--- activitystream/serializers.py
+++ activitystream/serializers.py
@@ -1,34 +1,41 @@
 from rest_framework import serializers
 
 
-class SubmissionSerializer(serializers.Serializer):
+DIT_NAMESPACE = 'dit:directoryFormsApi'
+DIT_SUBMISSION_NAMESPACE = f'{DIT_NAMESPACE}:Submission'
 
-    DIT_NAMESPACE = 'dit:directoryFormsApi'
-    DIT_SUBMISSION_NAMESPACE = f'{DIT_NAMESPACE}:Submission'
+
+class SubmissionSerializer(serializers.Serializer):
 
     def to_representation(self, obj):
         return {
-            'id': f'{self.DIT_SUBMISSION_NAMESPACE}:{obj.id}:Create',
+            'id': f'{DIT_SUBMISSION_NAMESPACE}:{obj.id}:Create',
             'type': 'Create',
             'published': obj.created.isoformat('T'),
-            'actor': {
-                'type': f'{self.DIT_SUBMISSION_NAMESPACE}:Sender',
-                'id': f'{self.DIT_NAMESPACE}:Sender:{obj.id}',
-                'dit:emailAddress': obj.sender.email_address,
-                'dit:isBlacklisted': obj.sender.is_blacklisted,
-                'dit:isWhitelisted': obj.sender.is_whitelisted,
-                'dit:blackListedReason': obj.sender.blacklisted_reason,
-            },
+            'actor': SenderSerializer(obj.sender).data,
             'object': {
-                'type': f'{self.DIT_SUBMISSION_NAMESPACE}',
-                'id': f'{self.DIT_SUBMISSION_NAMESPACE}:{obj.id}',
-                f'{self.DIT_SUBMISSION_NAMESPACE}:Meta': obj.meta,
-                f'{self.DIT_SUBMISSION_NAMESPACE}:Data': obj.data,
+                'type': f'{DIT_SUBMISSION_NAMESPACE}',
+                'id': f'{DIT_SUBMISSION_NAMESPACE}:{obj.id}',
+                f'{DIT_SUBMISSION_NAMESPACE}:Meta': obj.meta,
+                f'{DIT_SUBMISSION_NAMESPACE}:Data': obj.data,
                 'published': obj.created.isoformat('T'),
                 'name': obj.form_url,
                 'attributedTo': {
-                    'type': f'{self.DIT_NAMESPACE}:SubmissionAction:{obj.action_name}',
-                    'id': f'{self.DIT_NAMESPACE}:SubmissionType:{obj.submission_type}'
+                    'type': f'{DIT_NAMESPACE}:SubmissionAction:{obj.action_name}',
+                    'id': f'{DIT_NAMESPACE}:SubmissionType:{obj.submission_type}'
                 },
             },
         }
+
+
+class SenderSerializer(serializers.Serializer):
+
+    def to_representation(self, obj):
+        return {
+                'type': f'{DIT_SUBMISSION_NAMESPACE}:Sender',
+                'id': f'{DIT_NAMESPACE}:Sender:{obj.id}',
+                'dit:emailAddress': obj.email_address,
+                'dit:isBlacklisted': obj.is_blacklisted,
+                'dit:isWhitelisted': obj.is_whitelisted,
+                'dit:blackListedReason': obj.blacklisted_reason,
+            }

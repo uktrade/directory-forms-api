74bfee14a779626655155661d1913eeb109f0b40
davidu1975 <david.urquhart@digital.trade.gov.uk>
2024-08-14 12:11:26 +0100

    fix tests

--- conf/settings.py
+++ conf/settings.py
@@ -90,9 +90,8 @@ REDIS_URL = env.redis_url
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-DATABASES = {
-    'default': dj_database_url.config()
-}
+DATABASES = {'default': dj_database_url.config(default=env.database_url)}
+
 # Caches
 CACHES = {
     'default': {

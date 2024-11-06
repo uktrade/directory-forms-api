809ca76bfd18b93ea7164b2de0cac2d48a2230ab
davidu1975 <david.urquhart@digital.trade.gov.uk>
2024-11-06 14:19:23 +0000

    flake8

--- conf/gunicorn.py
+++ conf/gunicorn.py
@@ -5,6 +5,6 @@ def post_fork(server, worker):
     patch_with_psycogreen_gevent()
     worker.log.info("Enabled async Psycopg2")
 
+
 def patch_with_psycogreen_gevent():
     patch_psycopg()
-
--- conf/tests/test_fork.py
+++ conf/tests/test_fork.py
@@ -8,33 +8,33 @@ import gunicorn.util
 
 logger = logging.getLogger(__name__)
 
+
 class dotdict(dict):
     """dot.notation access to dictionary attributes"""
+
     __getattr__ = dict.get
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
 
 
-
 @pytest.fixture
 def worker():
     my_dict = {
-        'max_requests': 1000,
-        'max_requests_jitter': 10,
-        'umask': 22,
-        'worker_tmp_dir': '/tmp',
-        'uid': 503,
-        'gid': 20,
-
+        "max_requests": 1000,
+        "max_requests_jitter": 10,
+        "umask": 22,
+        "worker_tmp_dir": "/tmp",
+        "uid": 503,
+        "gid": 20,
     }
     cfg_dict = dotdict(my_dict)
-    
+
     worker = GeventWorker(
         log=logger,
         age=1,
         ppid=9999,
         sockets=[],
-        app='directory_forms_api', 
+        app="directory_forms_api",
         timeout=30,
         cfg=cfg_dict,
     )
@@ -45,17 +45,16 @@ def worker():
 def server():
     def handler():
         return None
-    
+
     server = StreamServer(
-        listener=(
-            '0.0.0.0:8020'
-        ),
+        listener=("0.0.0.0:8020"),
         handle=handler,
-    )  
+    )
     return server
 
-@patch('conf.gunicorn.patch_with_psycogreen_gevent')
-@patch.object(gunicorn.util, 'chown')
+
+@patch("conf.gunicorn.patch_with_psycogreen_gevent")
+@patch.object(gunicorn.util, "chown")
 def test_post_fork(mock_chown, mock_patch_with_psycogreen_gevent, worker, server):
     post_fork(server, worker)
     mock_patch_with_psycogreen_gevent.assert_called_once()

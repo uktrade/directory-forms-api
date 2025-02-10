5c2998c3a3db3927d224fbe9ac56753c722dbe61
Jack Vanderpump <jack@pumpymusic.co.uk>
2025-02-10 11:53:42 +0000

    add more env vars with default values

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -16,5 +16,18 @@ export gov_notify_letter_api_key=""
 export activity_stream_access_key_id=""
 export activity_stream_secret_access_key=""
 export database_url=""
+export authbroker_client_id=""
+export authbroker_client_secret=""
+export elastic_apm_secret_token=""
+export elastic_apm_url=""
+export health_check_token=""
+export zendesk_subdomain=""
+export zendesk_subdomain_euexit=""
+export zendesk_token=""
+export zendesk_email=""
+export zendesk_custom_field_id=""
+export secret_key=""
+export staff_sso_authbroker_url=""
+
 # Add commands below to run inside the container after all the other buildpacks have been applied
 python manage.py collectstatic --noinput

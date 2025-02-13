fd34a326c80373d1474ab99deb9335bfb08abbcf
davidu1975 <david.urquhart@digital.trade.gov.uk>
2025-02-13 13:20:23 +0000

    fix failing test

--- submission/tests/test_serializers.py
+++ submission/tests/test_serializers.py
@@ -272,39 +272,3 @@ def test_gov_notify_letter_action_serializer_from_submission(
         },
     }
 
-
-@pytest.mark.django_db
-def test_domestic_hcsat_feedback_serializer(hcsat_instance):
-
-    serializer = serializers.ActivityStreamDomesticHCSATUserFeedbackDataSerializer()
-    output = serializer.to_representation(hcsat_instance)
-
-    # Remove date due to timezone mismatch
-
-    del output["object"]["feedback_submission_date"]
-    expected = {
-        "id": f"dit:domestic:HCSATFeedbackData:{hcsat_instance.id}:Update",
-        "type": "Update",
-        "object": {
-            "id": hcsat_instance.data["id"],
-            "type": "dit:domestic:HCSATFeedbackData",
-            "url": hcsat_instance.data["url"],
-            "user_journey": hcsat_instance.data["user_journey"],
-            "satisfaction_rating": hcsat_instance.data["satisfaction_rating"],
-            "experienced_issues": hcsat_instance.data["experienced_issues"],
-            "other_detail": hcsat_instance.data["other_detail"],
-            "service_improvements_feedback": hcsat_instance.data[
-                "service_improvements_feedback"
-            ],
-            "likelihood_of_return": hcsat_instance.data["likelihood_of_return"],
-            "service_name": hcsat_instance.data["service_name"],
-            "service_specific_feedback": hcsat_instance.data[
-                "service_specific_feedback"
-            ],
-            "service_specific_feedback_other": hcsat_instance.data[
-                "service_specific_feedback_other"
-            ],
-        },
-    }
-
-    assert output == expected
